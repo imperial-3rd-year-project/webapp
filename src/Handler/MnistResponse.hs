8a3ad1629f039fb18930a14ffd4a6301fd507656
Gabriella Marfani <gm518@ic.ac.uk>
2020-10-26 13:53:52 +0000

    Gabs: handles json with value ish working

--- src/Handler/MnistResponse.hs
+++ src/Handler/MnistResponse.hs
@@ -6,16 +6,16 @@ import Data.Aeson
 import Yesod.Core.Json
 import Data.ByteString
 
-newtype MnistResponse = MnistResponse ByteString
+-- newtype MnistResponse = MnistResponse ByteString
 
-instance ToJSON MnistResponse where
-    toJSON (MnistResponse t) = object ["canvasData" .= t]
-instance FromJSON MnistResponse where
-    parseJSON = withObject "MnistResponse" $ \o -> MnistResponse <$> o .: "canvasData"
+-- instance ToJSON MnistResponse where
+--     toJSON (MnistResponse t) = object ["canvasData" .= t]
+-- instance FromJSON MnistResponse where
+--     parseJSON = withObject "MnistResponse" $ \o -> MnistResponse <$> o .: "canvasData"
 
 postMnistResponseR :: Handler Value
 postMnistResponseR = do
     -- requireCheckJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
-    response <- requireCheckJsonBody :: Handler MnistResponse
+    response <- requireCheckJsonBody :: Handler Value
 
     returnJson response
