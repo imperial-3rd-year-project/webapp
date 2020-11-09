8f9233e4c0fe79b104fc074768d13cba049dd90e
Aporva Varshney <av918@ic.ac.uk>
2020-11-09 20:10:09 +0000

    Add syntax highlighting to code examples, some styling changes and clean unused code

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -127,6 +127,8 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_prism_css
+            addScript     $ StaticR js_prism_js
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
--- src/Handler/MnistResponse.hs
+++ src/Handler/MnistResponse.hs
@@ -6,13 +6,11 @@
 
 module Handler.MnistResponse where
 
-import qualified Data.Vector.Storable  as V
 import Data.Vector.Storable.ByteString        (byteStringToVector)
 import Grenade.Assets.Paths
 import Grenade.Demos.MNIST
 import Import                          hiding (Vector)
 import Data.ByteString.Base64
-import Data.Either (fromRight)
 
 newtype MnistResponse = MnistResponse {unMnistResponse :: Text}
   deriving Show
@@ -28,7 +26,5 @@ postMnistResponseR = do
       Right image'' = decode image'
       image         = byteStringToVector image''
   traceM $  show $ V.length image
-  --liftIO $ Prelude.putStrLn "aaaaaa help me please"
-  --liftIO $ Prelude.putStrLn $ show $ decode image'
   return $ toJSON $ runNet' net image 224
 
