9d6b45a4ebae389ecf3f9ae29239ea10f37b389f
Aporva Varshney <av918@ic.ac.uk>
2020-11-10 00:04:06 +0000

    Decrease canvas resolution to allow transfer of canvas data

--- electron/main.js
+++ electron/main.js
@@ -3,9 +3,8 @@ const { spawn } = require('child_process');
 
 function createWindow () {
   const mainWindow = new BrowserWindow({ backgroundColor: "#000" });
+  const child  = spawn(".stack-work/dist/x86_64-linux-tinfo6/Cabal-3.0.1.0/build/web-app/web-app");
   mainWindow.loadURL("http://localhost:3000");
-  // const child    = spawn(".stack-work/dist/x86_64-linux-tinfo6/Cabal-3.0.1.0/build/web-app/web-app");
-  const child = spawn("stack", ["exec", "yesod", "--", "devel"]);
   child.stderr.on('data', (data) => { console.error(`stderr: ${data}`); });
   child.stdout.on('data', (data) => { console.log(`data: ${data}`);});
   mainWindow.on('close', () => child.kill());
