457fe34d4d9bcca4b44a2d07405bfdc934eccda7
Aporva Varshney <av918@ic.ac.uk>
2020-11-12 18:07:22 +0000

    Fix error in usage of new network loading function

--- src/Handler/MnistResponse.hs
+++ src/Handler/MnistResponse.hs
@@ -9,6 +9,7 @@ module Handler.MnistResponse where
 import Data.Vector.Storable.ByteString        (byteStringToVector)
 import Grenade.Assets.Paths
 import Grenade.Demos.MNIST
+import Grenade.Assets.Paths
 import Import                          hiding (Vector)
 import Data.ByteString.Base64
 import qualified Debug.Trace as DB
@@ -20,8 +21,8 @@ newtype MnistResponse = MnistResponse {unMnistResponse :: Text}
 postMnistResponseR :: Handler Value
 postMnistResponseR = do
   response  <- requireCheckJsonBody
-  mnistPath <- getPathForNetwork MNIST
-  (net :: MNIST) <- liftIO $ loadNetwork mnistPath
+  mnistPath <- liftIO $ getPathForNetwork MNIST
+  net <- liftIO $ (loadNetwork mnistPath :: IO MNIST)
   let image'        = encodeUtf8 $ unMnistResponse response
       Right image'' = decode image'
       image         = byteStringToVector image''
