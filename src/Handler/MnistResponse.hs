cacceb31d95bb3d498872bc3b34d1e6b5c8d0ed3
Aporva Varshney <av918@ic.ac.uk>
2020-11-16 16:09:59 +0000

    Use git submodules for fetching tutorials and other dependencies

--- src/Handler/MnistResponse.hs
+++ src/Handler/MnistResponse.hs
@@ -8,7 +8,8 @@ module Handler.MnistResponse where
 
 import qualified Data.Vector.Storable  as V
 import Data.Vector.Storable.ByteString        (byteStringToVector)
-import Grenade.Canvas.Helpers
+import Grenade.Assets.Paths
+import Grenade.Demos.MNIST
 import Import                          hiding (Vector)
 import Data.ByteString.Base64
 import Data.Either (fromRight)
@@ -21,12 +22,13 @@ postMnistResponseR :: Handler Value
 postMnistResponseR = do
   return "test the circle"
   response <- requireCheckJsonBody
-  net      <- liftIO $ netLoad "/Users/gabymarfani/Desktop/imperial/year_3/group_project/grenade/models/mnistModel"
+  mnistPath <- liftIO $ getPathForNetwork MNIST
+  net       <- liftIO $ loadNetwork mnistPath
   let image'        = encodeUtf8 $ unMnistResponse response
       Right image'' = decode image'
       image         = byteStringToVector image''
   traceM $  show $ V.length image
   --liftIO $ Prelude.putStrLn "aaaaaa help me please"
   --liftIO $ Prelude.putStrLn $ show $ decode image'
-  return $ toJSON $ runNet' net image
+  return $ toJSON $ runNet' net image 224
 
--- src/Handler/RunCode.hs
+++ src/Handler/RunCode.hs
@@ -13,6 +13,7 @@ import System.IO.Temp
 import System.Process
 import GHC.IO.Handle
 import System.FilePath.Posix
+import System.Directory ( getCurrentDirectory )
 
 -- import           Control.Monad
 -- import           Control.Monad.Random
@@ -32,10 +33,14 @@ postRunCodeR = do
     -- requireCheckJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
     code <- requireCheckJsonBody :: Handler UserCode
     let contents = (unUserCode code)
-    tmpFile <- liftIO $ writeSystemTempFile "tmpTut.hs" contents
-    let (dir, file) = splitFileName tmpFile
+    
+    -- tmpFile <- liftIO $ writeSystemTempFile "tmpTut.hs" contents
+    cwd <- liftIO $ getCurrentDirectory
+    let cwd' = cwd ++ "/grenade-tutorials"
+    let tmpFile = cwd' ++ "/src/circle-user.hs"
     liftIO $ print tmpFile
-    (_, hout, _ , _) <- liftIO $ createProcess (shell ("runghc "++ file)){ cwd = Just dir, std_out = CreatePipe}
+    liftIO $ writeFile tmpFile contents
+    (_, hout, _ , _) <- liftIO $ createProcess (shell ("stack run circle-user")){ cwd = Just cwd', std_out = CreatePipe}
     res <- do
         case hout of 
             (Just h) -> liftIO $ hGetContents h
