23523879c2755b895b0654619d123c8fcf6d64f9
Roko <rp218@ic.ac.uk>
2021-01-01 21:22:33 +0100

    Fixes test to work with the changes made

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -11,15 +11,16 @@ spec = withApp $ do
       it "loads the index and checks it looks right" $ do
           get HomeR
           statusIs 200
-          htmlAnyContain "h1" "a modern framework for blazing fast websites"
+          htmlAnyContain "h1" "Grenade"
 
-          request $ do
-              setMethod "POST"
-              setUrl HomeR
-              addToken
-              fileByLabelExact "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
-              byLabelExact "What's on the file?" "Some Content"
+          -- Example test of file upload; not on the main page anymore
+          -- request $ do
+          --     setMethod "POST"
+          --     setUrl HomeR
+          --     addToken
+          --     fileByLabelExact "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
+          --     byLabelExact "What's on the file?" "Some Content"
 
-          -- more debugging printBody
-          htmlAllContain ".upload-response" "text/plain"
-          htmlAllContain ".upload-response" "Some Content"
+          -- -- more debugging printBody
+          -- htmlAllContain ".upload-response" "text/plain"
+          -- htmlAllContain ".upload-response" "Some Content"
